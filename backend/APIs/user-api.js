480f1683efc7cce24743972e35376a7c3fa1614f
GKA-exe <krishna.gunturi03@gmail.com>
2024-06-18 14:25:04 +0530

    user and author backend done

--- backend/APIs/user-api.js
+++ backend/APIs/user-api.js
@@ -3,9 +3,36 @@ const userApp = exp.Router();
 const { createUserOrAuthor, userorAuthorLogin } = require("./util");
 const expressAsyncHandler = require("express-async-handler");
 
+let articlesCollection;
+let usersCollection;
+userApp.use((req, res, next) => {
+    usersCollection = req.app.get("usersCollection")
+    articlesCollection = req.app.get("articlesCollection");
+    next();
+})
+
 userApp.post("/register", expressAsyncHandler (createUserOrAuthor));
 
 userApp.post("/login", expressAsyncHandler (userorAuthorLogin));
 
+userApp.get("/articles", expressAsyncHandler(async (req, res) => {
+    const articlesList = await articlesCollection.find({status: true}).toArray()
+
+    res.send({ message: "Articles List", payload:  articlesList  });
+}))
+
+// write comments
+userApp.post("/comment/:articleId", expressAsyncHandler(async (req, res) => {
+    // get article id from url
+    const articleIdFromUrl = req.params.articleId;
+    // get commentobj from req
+    const userComment = req.body;
+    // get article document
+    // add the comment in the array
+    // update the document
+    // all can be done in single step using $addToSet operator in mongo
+    await articlesCollection.updateOne({ article: articleIdFromUrl }, { $addToSet: { comments: userComment } })
+    res.send({message: "comment added!"})
+}))
 
 module.exports = userApp;
